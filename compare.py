5c68ee9354d721e26daa4be6d77dae14ec8e4d79
Sameer Achhab <82197472+Dfmaaa@users.noreply.github.com>
2021-04-22 14:38:35 +0530

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,405 @@
+print("type 2 numbers. This app will compare them.")
+Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ print("type 2 numbers. This app will compare them.")
+Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ print("type 2 numbers. This app will compare them.")
+Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ print("type 2 numbers. This app will compare them.")
+Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ print("type 2 numbers. This app will compare them.")
+Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ print("type 2 numbers. This app will compare them.")
+Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ print("type 2 numbers. This app will compare them.")
+Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ print("type 2 numbers. This app will compare them.")
+Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ print("type 2 numbers. This app will compare them.")
+Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ r=("close")
+ f=input(("type close to exit"))
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ Num1=float(input("Type a number:"))
+Num2=float(input("Type another number:"))
+if Num1>Num2:
+ print("The first Number is bigger.")
+elif(Num2>Num1):
+ print("The second number is bigger.")
+else:
+ print("The numbers are equal")
+ 
+ 
+ 
